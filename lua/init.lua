2e041e4330713e07b0f74601ecad676202cab01d
devbyte1328 <devbyte1328@gmail.com>
2026-01-26 13:59:33 +0200

    Add 'init.lua' with a basic hello world script

---
+++ lua/init.lua
@@ -0,0 +1,29 @@
+-- Main module for the Hello World plugin
+local M = {}
+
+-- Function to print the hello message
+function M.say_hello()
+	print("Hello from Neovim! SuperCodex!")
+end
+
+-- Function to set up the plugin
+function M.setup(opts)
+	-- Merge user options with defaults
+	opts = opts or {}
+
+	-- Create the user command
+	vim.api.nvim_create_user_command("HelloWorld", M.say_hello, {})
+
+	-- Set up a key mapping
+	-- Use opts.keymap if provided, otherwise default to '<leader>hw'
+	local keymap = opts.keymap or "<leader>hw"
+
+	-- Create the keymap
+	vim.keymap.set("n", keymap, M.say_hello, {
+		desc = "Say hello from our plugin",
+		silent = true, -- Prevents the command from being echoed in the command line
+	})
+end
+
+-- Return the module
+return M

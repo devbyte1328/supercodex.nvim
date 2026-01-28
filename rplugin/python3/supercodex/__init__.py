2828b140a62f4c27e430e5a5d7608847712a7ddc
devbyte1328 <devbyte1328@gmail.com>
2026-01-28 15:18:25 +0200

    Add boilerplate code for openning window with input box

--- rplugin/python3/supercodex/__init__.py
+++ rplugin/python3/supercodex/__init__.py
@@ -8,3 +8,23 @@ class Hello:
     @pynvim.autocmd("VimEnter", pattern="*", sync=False)
     def on_start(self):
         self.nvim.out_write("Hello World! SuperCodex! Python!\n")
+
+    @pynvim.command("HelloWindow", nargs=0, sync=True)
+    def hello_window(self):
+        input_buffer = self.nvim.api.create_buf(False, True)
+
+        self.nvim.api.buf_set_option(input_buffer, "buftype", "prompt")
+        self.nvim.funcs.prompt_setprompt(input_buffer, "> ")
+
+        self.nvim.api.open_win(
+            input_buffer,
+            True,
+            {
+                "relative": "editor",
+                "row": 10,
+                "col": 10,
+                "width": 40,
+                "height": 1,
+                "style": "minimal",
+            },
+        )

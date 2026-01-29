0c5a309675df2b9256cc1b65eee9b0d7ffeba8e2
devbyte1328 <devbyte1328@gmail.com>
2026-01-29 10:04:40 +0200

    Save and print the text input in window prompt

--- rplugin/python3/supercodex/__init__.py
+++ rplugin/python3/supercodex/__init__.py
@@ -7,6 +7,31 @@ class SuperCodex:
     def __init__(self, nvim):
         self.nvim = nvim
 
+    @pynvim.function("SubmitPrompt", sync=True)
+    def prompt_submit(self, args):
+        text = args[0]
+
+        # NeoVim makes it difficult to print the text input in a callback,
+        # so Vimscript and a NeoVim global variable are used as a bridge.
+
+        # Vimscript that is used to print the text
+        self.nvim.command("""
+        function! SuperCodexPrintMsg(timer) abort
+          echomsg g:supercodex_msg
+        endfunction
+        """)
+
+        # Store text in NeoVim's global variables
+        self.nvim.vars["supercodex_msg"] = text
+
+        # exit the window
+        self.nvim.command("stopinsert")
+        self.nvim.command("bwipeout!")
+
+        # print AFTER callback returns using the Vimscript function
+        self.nvim.command("call timer_start(0, function('SuperCodexPrintMsg'))")
+
+
     @pynvim.command("WindowInputPrompt", nargs=0, sync=True)
     def window_input_prompt(self):
         buffer = self.nvim.api.create_buf(False, True)
@@ -23,6 +48,7 @@ class SuperCodex:
         self.nvim.api.buf_set_keymap(buffer, "n", "q", close_cmd, {"silent": True, "nowait": True})
 
         self.nvim.funcs.prompt_setprompt(buffer, "> ")
+        self.nvim.funcs.prompt_setcallback(buffer, "SubmitPrompt")
 
         self.nvim.api.open_win(
             buffer,

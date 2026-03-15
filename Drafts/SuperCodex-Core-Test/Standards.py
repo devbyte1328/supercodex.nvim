86dc0d35b817785ad2043b7a91006ad559b5e556
devbyte1328 <devbyte1328@gmail.com>
2026-03-15 06:24:31 +0200

    Remove debug print from 'Standards.py'

--- Drafts/SuperCodex-Core-Test/Standards.py
+++ Drafts/SuperCodex-Core-Test/Standards.py
@@ -165,7 +165,6 @@ def Requests_post(url, headers, json):
     response_status_code = response.status_code
     response_json = response.json()
     response = response_json["choices"][0]["message"]["content"]
-    print(response_json)
     return response, response_status_code, response_json
 
 
